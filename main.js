d89be3effaa0df4600dcfe87f3532a4bb29fe760
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2020-07-30 18:37:40 -0500

    Added a prompt to receive alias or generation number

--- main.js
+++ main.js
@@ -3,7 +3,11 @@ const BACKEND_HOST = process.env.BACKEND_HOST || "http://localhost:5000";
 const axios = require("axios");
 const { main } = require("./utils/createRepo.js");
 
-const generationNumber = 69;
+const prompt = require("prompt-sync")({ sigint: true });
+
+const generationNumber = prompt(
+  "Write the Prepadawans generation (or alias): "
+);
 
 axios
   .get(`${BACKEND_HOST}/getChallenges?generationNumber=${generationNumber}`)
