c5c65f5f02eaab881ed25c6728a295c469875ef8
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2020-07-30 18:08:46 -0500

    Reformated to get the challenges info from a backend
    - We decided to get the challenges from the backend, as we are willing
      to publish the package to npm, and we need a secure connection to the
      database.

--- main.js
+++ main.js
@@ -1,41 +1,11 @@
-const dotenv = require("dotenv");
-dotenv.config();
+const BACKEND_HOST = process.env.BACKEND_HOST || "http://localhost:5000";
 
-const { connect2db } = require("./db/db.js");
+const axios = require("axios");
+const { main } = require("./utils/createRepo.js");
 
-const map = require("awaity/map");
-const { each } = require("awaity/esm");
+const generationNumber = 69;
 
-const { getExercises } = require("./db/utils.js");
-const { createRepo, createChallengesFolder } = require("./utils/createRepo.js");
-
-connect2db()
-  .then(async dbConnection => {
-    console.log("Connected to the DB");
-    try {
-      const difficulties = [
-        "very easy",
-        "easy",
-        "medium",
-        "hard",
-        "very hard",
-        "expert",
-      ];
-
-      const exercises = await map.default(
-        difficulties,
-        async difficulty => await getExercises(difficulty)
-      );
-
-      const folderName = createChallengesFolder(7);
-
-      await each(exercises, async (exercise, idx) => {
-        await createRepo(exercise, idx, folderName);
-      });
-    } catch (error) {
-      console.log(error);
-    } finally {
-      dbConnection.disconnect(() => console.log("Disconnected from the db"));
-    }
-  })
-  .catch(err => console.log(err));
+axios
+  .get(`${BACKEND_HOST}/getChallenges?generationNumber=${generationNumber}`)
+  .then(({ data }) => main(data.files, generationNumber))
+  .catch(error => console.error(error));
--- utils/createRepo.js
+++ utils/createRepo.js
@@ -1,58 +1,37 @@
 const fs = require("fs");
-const TurndownService = require("turndown");
 
-const html2md = (html, challengeNumber) => {
-  const turndownService = new TurndownService();
-  const markdown = turndownService.turndown(html);
-  return `# Challenge ${challengeNumber + 1}\n${markdown}`;
+module.exports.main = (exercises, generationNumber) => {
+  const rootFolder = module.exports.createChallengesFolder(generationNumber);
+  exercises.forEach(exercise => {
+    module.exports.createRepo(exercise, rootFolder);
+  });
 };
 
-module.exports.parseStarterFn = starterFn => {
-  const expression = / .*\(.*\)/;
-  const functions = starterFn.join("").match(expression);
-  const fnSignature = functions.map(fn => fn.split("("));
-  const starterCode = fnSignature
-    .map(
-      ([fnName, fnParams]) =>
-        `module.exports.${fnName.trim()} = (${fnParams} => {\n// Your amazing code here 🚀!\n}`
-    )
-    .join("\n");
-  const fnNames = fnSignature.map(([fnName]) => fnName.trim());
-  return { starterCode, fnNames };
-};
+module.exports.createRepo = (exercise, rootFolder) => {
+  const {
+    challengeNumber,
+    challengeIndex,
+    starterCodeFile,
+    testFile,
+    readmeFile,
+  } = exercise;
+  console.log(`Challenge #${challengeNumber}`);
 
-module.exports.parseTests = (tests, fnNames, challengeNumber) => {
-  const importStatements = `const { ${fnNames.join(
-    ", "
-  )} } = require("./challenge${challengeNumber + 1}.js");`;
-  const parsedTests = tests.join("\n");
-  return `${importStatements}\n\n${parsedTests}`;
-};
-
-module.exports.createRepo = (exercise, challengeNumber, rootFolder) => {
-  console.log(`Challenge #${challengeNumber + 1}`);
-  console.log(`Creating folder for ${exercise.title}`);
-  const folderName = `Challenge${challengeNumber + 1}`;
+  const folderName = `Challenge${challengeNumber}`;
   const localPath = `${rootFolder}/${folderName}`;
+
   fs.mkdirSync(localPath, { recursive: true });
 
-  fs.writeFileSync(
-    `${localPath}/README.md`,
-    html2md(exercise.description, challengeNumber)
-  );
-
-  const { starterCode, fnNames } = module.exports.parseStarterFn(
-    exercise.starterFn
-  );
+  fs.writeFileSync(`${localPath}/README.md`, readmeFile);
 
   fs.writeFileSync(
-    `${localPath}/challenge${challengeNumber + 1}.js`,
-    starterCode
+    `${localPath}/challenge${challengeNumber}.js`,
+    starterCodeFile
   );
 
   fs.writeFileSync(
-    `${localPath}/challenge${challengeNumber + 1}.test.js`,
-    module.exports.parseTests(exercise.tests, fnNames, challengeNumber)
+    `${localPath}/challenge${challengeNumber}.test.js`,
+    testFile
   );
 };
 
