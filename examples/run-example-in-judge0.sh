f607523109d9326982ea72fb48249861866134bd
Rodrigo Medina <rodrigom@lyft.com>
2022-05-27 23:45:20 -0500

    Improved example script

--- examples/run-example-in-judge0.sh
+++ examples/run-example-in-judge0.sh
@@ -14,15 +14,20 @@ JUDGE0_HOST="https://${JUDGE0_HOSTNAME}"
 
 # Create the zip file with the quiet flag, and the - indicates that the zip will be
 # piped as stdout to the next command
-echo "Creating the zip file with: index.js test.js run"
+echo -e "Creating the zip file with:\033[1m index.js test.js run\033[0m"
 additional_files=$(zip -q - index.js test.js run | base64)
 
 echo "Creating the submission"
 # Create the submission with the additional files, and pipe the result through jq to
 # get the submission token
-token=$(curl -s -X POST -H "Content-Type: application/json" -H "X-Auth-Token: ${JUDGE0_AUTH_TOKEN}" -d "{\"language_id\": 89, \"additional_files\": \"$additional_files\"}" "$JUDGE0_HOST/submissions?wait=false" | jq -r ".token")
+token=$(curl -s -X POST \
+  -H "Content-Type: application/json" \
+  -H "X-Auth-Token: ${JUDGE0_AUTH_TOKEN}" \
+  -d "{\"language_id\": 89, \"additional_files\": \"$additional_files\"}" \
+  "$JUDGE0_HOST/submissions?wait=false" \
+  | jq -r ".token")
 
-echo "Created submission with token: $token"
+echo -e "Created submission with token: \033[1m$token\033[0m"
 
 # Loop until the submission is finished
 while true; do
@@ -30,7 +35,10 @@ while true; do
 
   # Get the submission status from the Judge0 API
   echo "Checking submission status..."
-  submission=$(curl -s -H "X-Auth-Token: ${JUDGE0_AUTH_TOKEN}" "$JUDGE0_HOST/submissions/$token" | jq)
+  submission=$(curl -s \
+    -H "X-Auth-Token: ${JUDGE0_AUTH_TOKEN}" \
+    "$JUDGE0_HOST/submissions/$token" \
+    | jq)
 
   # Parse the status and show the status description
   statusId=$(echo $submission | jq -r ".status.id")
@@ -38,11 +46,11 @@ while true; do
 
   # If the status is 'In Queue' or 'Processing', keep looping
   if [ $statusId = 1 ] || [ $statusId = 2 ]; then
-    echo "Current submission status: $statusDescription"
+    echo -e "Current submission status: \033[1m\033[35m$statusDescription\033[0m"
     continue
   # Otherwise we can continue with the results of the submission
   else
-    echo "Done!"
+    echo -e "\033[1m\033[32mDone!\033[0m"
     break
   fi
 done
