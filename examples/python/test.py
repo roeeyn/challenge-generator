90c960e4a3dbe2d8ad09aca6c123b8d5fa38fcc2
Rodrigo Medina <rodrigom@lyft.com>
2022-05-30 00:09:19 -0500

    Added and updated python examples

--- examples/python/test.py
+++ examples/python/test.py
@@ -1,9 +1,4 @@
-from index import hello_world
-import unittest
-
-class TestHello(unittest.TestCase):
-    def test_hello_world(self):
-        assert hello_world() == "Hello World!"
-
-if __name__ == '__main__':
-    unittest.main()
+Test.assert_equals(validate_binary('00101101'), True)
+Test.assert_equals(validate_binary('11000000'), True)
+Test.assert_equals(validate_binary('11000001'), False)
+Test.assert_equals(validate_binary('10010010'), False)
